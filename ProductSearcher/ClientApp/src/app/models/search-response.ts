1c0b2e40135baf090533cc9c38f115ee95946495
Sergio <lsv.sergio@gmail.com>
2021-11-24 20:55:01 +0200

    Delete ProductSearcher directory

--- ProductSearcher/ClientApp/src/app/models/search-response.ts
+++
@@ -1,8 +0,0 @@
-import {Product} from "./product";
-
-export interface SearchResponse {
-
-	products: Product[];
-	storeName: string;
-
-}
--- ProductSearcher/ClientApp/src/app/services/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './search.service';
-export * from './shops.service';
-export * from './message-bus.service';
